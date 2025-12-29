b1496d97df20f93f2b65e5349871c9c89d56695e
s-afrid <afrid0052@gmail.com>
2025-12-29 17:28:46 +0530

    fix: update background color of Navbar and Footer components to gray

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-slate-900 text-white flex justify-center px-4 h-16 items-center'>
+    <footer className='bg-gray-900 text-white flex justify-center px-4 h-16 items-center'>
       <p className='text-center'>Copyright &copy; Get me Chai - All rights reserved</p>
     </footer>
   )
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-slate-900 text-white flex justify-between px-4 h-16 items-center'>
+    <nav className='bg-gray-900 text-white flex justify-between px-4 h-16 items-center'>
         <div className="logo font-bold text-lg">Get Me Chai</div>
         <ul className='flex justify-between gap-4'>
             <li>Home</li>
