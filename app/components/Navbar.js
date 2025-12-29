c297f6f2ffd3fca28a97e03f5f83e00c799456df
s-afrid <afrid0052@gmail.com>
2025-12-29 17:13:57 +0530

    starter class for navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-        Navbar
+    <nav className='bg-black text-white flex justify-between px-4 h-16 items-center'>
+        <div className="logo font-bold">GetMeChai</div>
+        <ul className='flex justify-between gap-4'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Sign Up</li>
+            <li>Login</li>
+        </ul>
     </nav>
   )
 }
