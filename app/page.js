37c013c146c5ef4397f1c0de78f06397b59e8703
s-afrid <afrid0052@gmail.com>
2025-12-30 11:00:02 +0530

    added gif assets

--- app/page.js
+++ app/page.js
@@ -11,18 +11,19 @@ export default function Home() {
         A crowdfunding platform for creators. Get funded by your fans and followers. Start Now!
       </p>
       <div>
-        <button type="button" class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5 rounded-3xl me-2 mb-2">Start Here</button>
-        <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5 rounded-3xl me-2 mb-2">Read More</button>
+        <button type="button" className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5 rounded-3xl me-2 mb-2">Start Here</button>
+        <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5 rounded-3xl me-2 mb-2">Read More</button>
       </div>
     </div>
 
     <div className="bg-white h-1 opacity-10"></div>
 
-    <div>
+    <div className="text-white">
       <h1>Your Fans can buy you a Chai</h1>
       <div className="flex gap-5">
-        <div className="item">
-          im
+        <div className="item bg-slate-200 rounded-full p-7 text-black">
+          <img width={88} src="/man.gif" alt="man" />
+          <p>Fund yourself</p>
         </div>
       </div>
     </div>
