3d1f7769b5091eeccbcb432da7982072913128a0
s-afrid <afrid0052@gmail.com>
2025-12-30 09:55:39 +0530

    feat: enhance Home page layout with title, description, and buttons

--- app/page.js
+++ app/page.js
@@ -2,8 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Home page
+    <>
+    <div className="flex flex-col justify-center items-center gap-4 text-white h-[44vh]">
+      <div className="font-bold text-5xl">
+        Buy me a Chai
+      </div>
+      <p>
+        A crowdfunding platform for creators. Get funded by your fans and followers. Start Now!
+      </p>
+      <div>
+        <button type="button" class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5 rounded-3xl me-2 mb-2">Start Here</button>
+        <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5 rounded-3xl me-2 mb-2">Read More</button>
+      </div>
     </div>
+    <div className="bg-white h-1 opacity-10">
+    
+    </div>
+    </>
+    
   );
 }
