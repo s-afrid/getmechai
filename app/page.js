e12fc828a26a643f139c501fc839a3bf6a28b8a7
s-afrid <afrid0052@gmail.com>
2025-12-30 10:15:19 +0530

    Refactor code structure for improved readability and maintainability

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <>
     <div className="flex flex-col justify-center items-center gap-4 text-white h-[44vh]">
-      <div className="font-bold text-5xl">
-        Buy me a Chai
+      <div className="font-bold text-5xl flex gap-3 justify-center items-center">
+        Buy me a Chai <span><img src="/tea.gif" width={88} alt="tea" /></span>
       </div>
       <p>
         A crowdfunding platform for creators. Get funded by your fans and followers. Start Now!
@@ -15,8 +15,16 @@ export default function Home() {
         <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5 rounded-3xl me-2 mb-2">Read More</button>
       </div>
     </div>
-    <div className="bg-white h-1 opacity-10">
-    
+
+    <div className="bg-white h-1 opacity-10"></div>
+
+    <div>
+      <h1>Your Fans can buy you a Chai</h1>
+      <div className="flex gap-5">
+        <div className="item">
+          im
+        </div>
+      </div>
     </div>
     </>
     
