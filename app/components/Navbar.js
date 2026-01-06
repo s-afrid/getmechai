b82bf83fdfa5855a357c73273379a61aaca59318
s-afrid <afrid0052@gmail.com>
2026-01-06 14:42:43 +0530

    created logout button with functionality

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -5,12 +5,7 @@ import Link from 'next/link'
 
 const Navbar = () => {
   const { data: session } = useSession()
-  if(session) {
-    return <>
-      Signed in as {session.user.email} <br/>
-      <button onClick={() => signOut()}>Sign out</button>
-    </>
-  }
+  
   return (
     <nav className='bg-gray-900 text-white flex justify-between px-4 h-16 items-center'>
         <div className="logo font-bold text-lg flex gap-3 items-center justify-center">
@@ -24,14 +19,22 @@ const Navbar = () => {
         <div>
           {session && <Link href={"/dashboard"}>
           <button className='text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5 rounded-3xl me-2 b-2'>
-            Login
+            Dashboard
           </button>
           </Link>}
-          <Link href={"/login"}>
+
+          {session && 
+          <button onClick={()=>{signOut()}} className='text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5 rounded-3xl me-2 b-2'>
+            Logout
+          </button>
+          }
+
+          {!session && <Link href={"/login"}>
           <button className='text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5 rounded-3xl me-2 b-2'>
             Login
           </button>
-          </Link>
+          </Link>}
+          
           
         </div>
     </nav>
--- app/login/page.js
+++ app/login/page.js
@@ -62,7 +62,7 @@ const page = () => {
   </button>
 
   {/* Github Button */}
-  <button onClick={()=>{signIn("github ")}} className="flex items-center w-64 bg-white border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+  <button onClick={()=>{signIn("github")}} className="flex items-center w-64 bg-white border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
     <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 73 73" version="1.1">
       <g id="team-collaboration/version-control/github" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
         <g id="container" transform="translate(2.000000, 2.000000)" fillRule="nonzero">
