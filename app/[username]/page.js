70458cecbc90525502eac404263e0899013737e6
s-afrid <afrid0052@gmail.com>
2026-01-07 12:56:23 +0530

    profilepic and cover structure

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,12 +1,22 @@
 import React from 'react'
 
 const Username = async ({params}) => {
-let username = await params
-username = username.username
+let getparams = await params
+let username = getparams.username
   return (
-    <div className=''>
-      {username}
+    <>
+
+    <div className='cover w-full relative'>
+      <img className='object-cover w-full h-[350px]' src="https://c10.patreonusercontent.com/4/patreon-media/p/campaign/812526/fb5da5add2ad4e41bd52d66611e0249c/eyJ3IjoxOTIwLCJ3ZSI6MX0%3D/1.JPG?token-hash=tWU41zyvAnKOa8SWj33OA0_nxl35cBso0nmDNC17CDI%3D&token-time=1770163200" alt="cover" />
+      <div className="profilepic absolute top-[79%] right-[46%] rounded-lg">
+        <img width={150} height={150} className='border-2 border-white rounded-full' src="https://c10.patreonusercontent.com/4/patreon-media/p/campaign/812526/a6d12b5556f149d0aa37df5e76d2be6f/eyJoIjozNjAsInciOjM2MH0%3D/2.JPG?token-hash=EfcYEKOhWRY4UZ9xYojuo-qejcwYT_3WJwjvjWqtLvw%3D&token-time=1769040000" alt="profilepic" />
+      </div>
     </div>
+
+    <div className="info flex items-center justify-center my-24">
+      @{username}
+    </div>
+    </>
   )
 }
 
