bfc05eb41ed2397cf510047370d6009f97fa8340
s-afrid <afrid0052@gmail.com>
2026-02-13 09:07:44 +0530

    added year in footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
+const currentYear = new Date().getFullYear();
+
   return (
     <footer className='bg-gray-900 text-white flex justify-center px-4 h-16 items-center'>
-      <p className='text-center'>Copyright &copy; Get me Chai - All rights reserved</p>
+      <p className='text-center'>Copyright &copy; {currentYear} - Get me Chai - All rights reserved</p>
     </footer>
   )
 }
